485b58208be86a495388c2ca618f13f4ca442558
leventerevesz <35273710+leventerevesz@users.noreply.github.com>
2019-03-08 21:38:46 +0100

    Add ArUco code, documentation

---
+++ aruco_markers.py
@@ -0,0 +1,23 @@
+import cv2 as cv
+from cv2 import aruco
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib as mpl
+
+def make_markers():
+    "Creates 4X4 ArUco Markers"
+    # Stolen code from the ArUco tutorial
+    aruco_dict = aruco.Dictionary_get(aruco.DICT_4X4_50)
+
+    fig = plt.figure()
+    nx = 4
+    ny = 3
+    for i in range(1, nx*ny+1):
+        ax = fig.add_subplot(ny,nx, i)
+        img = aruco.drawMarker(aruco_dict,i, 600)
+        plt.imshow(img, cmap = mpl.cm.gray, interpolation = "nearest")
+        ax.axis("off")
+    plt.savefig("results//ArUco_Markers.pdf")
+    plt.show()
+
+make_markers()
